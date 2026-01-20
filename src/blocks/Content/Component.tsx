f64b290c5d199d879b11b579f06b299c99bfc1f9
chuangcaleb <62867911+chuangcaleb@users.noreply.github.com>
2026-01-20 20:53:51 +0800

    ui: extra spacing

--- src/blocks/Content/Component.tsx
+++ src/blocks/Content/Component.tsx
@@ -17,7 +17,7 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
   }
 
   return (
-    <div className="container my-16">
+    <div className="container my-28">
       <div className="grid grid-cols-4 lg:grid-cols-12 gap-y-8 gap-x-16">
         {columns &&
           columns.length > 0 &&
@@ -26,7 +26,7 @@ export const ContentBlock: React.FC<ContentBlockProps> = (props) => {
 
             return (
               <div
-                className={cn(`col-span-4 lg:col-span-${colsSpanClasses[size!]}`, {
+                className={cn(`space-y-6 col-span-4 lg:col-span-${colsSpanClasses[size!]}`, {
                   'md:col-span-2': size !== 'full',
                 })}
                 key={index}
